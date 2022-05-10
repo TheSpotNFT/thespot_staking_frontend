84b73e0f2d6d2fa5984e89678dd4de04678e14f8
thejunglemarket <91908299+thejunglemarket@users.noreply.github.com>
2022-05-09 20:05:12 -0700

    more staking items

--- src/StakingCollections.js
+++ src/StakingCollections.js
@@ -20,6 +20,18 @@ import image613 from "./images/613.png";
 import image621 from "./images/621.png";
 import image624 from "./images/624.png";
 import image629 from "./images/629.png";
+import image143 from "./images/143.png";
+import image127 from "./images/127.png";
+import image209 from "./images/209.png";
+import image332 from "./images/332.png";
+import image339 from "./images/339.png";
+import image431 from "./images/431.png";
+import image423 from "./images/423.png";
+import image620 from "./images/620.png";
+import image623 from "./images/623.png";
+import image625 from "./images/625.png";
+import image637 from "./images/637.png";
+import image643 from "./images/643.png";
 
 const collection = [
   {
@@ -253,6 +265,138 @@ const collection = [
     stakingTokenId: "629",
     rewardName: "Itsa Mee Headwear",
     stakingTime: "24 hrs",
+  },
+  {
+    image: image143,
+    id: "21",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "19",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "143",
+    rewardName: "Pins",
+    stakingTime: "7 days",
+  },
+  {
+    image: image127,
+    id: "22",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "20",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "127",
+    rewardName: "Iron Goat",
+    stakingTime: "2 days",
+  },
+  {
+    image: image209,
+    id: "23",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "21",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "209",
+    rewardName: "Brown Head",
+    stakingTime: "24 hours",
+  },
+  {
+    image: image332,
+    id: "24",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "22",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "332",
+    rewardName: "Robogoat Eyes",
+    stakingTime: "7 days",
+  },
+  {
+    image: image339,
+    id: "25",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "23",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "339",
+    rewardName: "Infinity Eyes",
+    stakingTime: "3 days",
+  },
+  {
+    image: image431,
+    id: "26",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "24",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "431",
+    rewardName: "Infinity Mouth",
+    stakingTime: "24 hrs",
+  },
+  {
+    image: image423,
+    id: "27",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "25",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "423",
+    rewardName: "Baller Mouth",
+    stakingTime: "3 days",
+  },
+  {
+    image: image620,
+    id: "28",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "26",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "620",
+    rewardName: "Fighter Headwear",
+    stakingTime: "48 hours",
+  },
+  {
+    image: image623,
+    id: "29",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "27",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "623",
+    rewardName: "Goatd Mania Headwear",
+    stakingTime: "48 hours",
+  },
+  {
+    image: image625,
+    id: "30",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "28",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "625",
+    rewardName: "HaHaHa Headwear",
+    stakingTime: "48 hours",
+  },
+  {
+    image: image637,
+    id: "31",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "29",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "637",
+    rewardName: "Cold Chillin Headwear",
+    stakingTime: "3 days",
+  },
+  {
+    image: image643,
+    id: "32",
+    name: "Spot",
+    contract: "0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8",
+    contractIndex: "30",
+    rewardContract: "0x9521807ADF320D1CDF87AFDf875Bf438d1D92d87",
+    stakingTokenId: "643",
+    rewardName: "Suited Up Headwear",
+    stakingTime: "5 days",
   },
 ];
 export default collection;
