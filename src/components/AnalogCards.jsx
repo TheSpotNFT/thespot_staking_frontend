7254bb6ccf1ad6a2fc1bee515909b5179888b666
thejunglemarket <91908299+thejunglemarket@users.noreply.github.com>
2022-07-09 21:48:13 -0700

    update analog for mobile

--- src/components/AnalogCards.jsx
+++ src/components/AnalogCards.jsx
@@ -5,6 +5,7 @@ function Card(props) {
   const { Moralis } = useMoralis();
   const contractProcessor = useWeb3ExecuteFunction();
   const [variation, setVariation] = useState(props.image1);
+  const [variationSelection, setVariationSelection] = useState("1");
   const { account, isAuthenticated } = useMoralis();
   const analogContract = "0xBe18CF471925d683c272AAFe9d1aaFDA99612B69";
   const userAddress = account;
@@ -20,18 +21,60 @@ console.log(variation2);
 
   function changeVariation1() {
     setVariation(props.image1);
+    setVariationSelection("1");
   }
   function changeVariation2() {
     setVariation(props.image2);
+    setVariationSelection("2");
   }
   function changeVariation3() {
     setVariation(props.image3);
+    setVariationSelection("3");
   }
   function changeVariation4() {
     setVariation(props.image4);
+    setVariationSelection("4");
   }
 
-  async function commitVariation1() {
+  async function commitVariation() {
+    let options = {
+      contractAddress: "0xBe18CF471925d683c272AAFe9d1aaFDA99612B69",
+      functionName: "changeVariation",
+      abi: [
+        {
+          inputs: [
+            { internalType: "uint256", name: "_tokenID", type: "uint256" },
+            { internalType: "uint256", name: "_variationID", type: "uint256" },
+          ],
+          name: "changeVariation",
+          outputs: [],
+          stateMutability: "payable",
+          type: "function",
+        },
+      ],
+      msgValue: Moralis.Units.ETH(1.0),
+      params: {
+        _tokenID: props.id,
+        _variationID: variationSelection,
+      },
+    };
+
+    await contractProcessor.fetch({
+      params: options,
+      onError: (err) => {
+        setIsLoading(false);
+        alert(JSON.stringify(err.data.message));
+      },
+      onSuccess: (tx) => {
+        tx.wait(5)
+          .then(alert("Variation Successful"))
+          .then(setIsLoading(false))
+          .then(console.log(tx));
+      },
+    });
+  }
+
+  /*async function commitVariation1() {
     let options = {
       contractAddress: "0xBe18CF471925d683c272AAFe9d1aaFDA99612B69",
       functionName: "changeVariation",
@@ -179,7 +222,7 @@ console.log(variation2);
           .then(console.log(tx));
       },
     });
-  }
+  }*/
   return (
     <div
       className="w-full rounded overflow-hidden shadow-lg bg-slate-700 hover: hover:scale-105 hover:bg-slate-500 duration-300"
@@ -198,7 +241,7 @@ console.log(variation2);
               className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
       hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l flex justify-center"
               onMouseEnter={changeVariation1}
-              onClick={commitVariation1}
+              onClick={changeVariation1}
             >
               1
             </button>
@@ -206,7 +249,7 @@ console.log(variation2);
               className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
       hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l flex justify-center"
               onMouseEnter={changeVariation2}
-              onClick={commitVariation2}
+              onClick={changeVariation2}
             >
               2
             </button>
@@ -215,7 +258,7 @@ console.log(variation2);
               className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
       hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l flex justify-center"
               onMouseEnter={changeVariation3}
-              onClick={commitVariation3}
+              onClick={changeVariation3}
             >
               3
             </button></div>
@@ -224,13 +267,21 @@ console.log(variation2);
               className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
       hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l flex justify-center"
               onMouseEnter={changeVariation4}
-              onClick={commitVariation4}
+              onClick={changeVariation4}
             >
               4
             </button></div>
           </div>
-          <div className="flex justify-center">
-            <h5>Click number of variaton to commit change to NFT</h5>
+          <div className="flex grid grid-cols-1 justify-center">
+         
+            <h5></h5> <button
+              className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
+      hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l flex justify-center"
+              
+              onClick={commitVariation}
+            >
+              Click to Commit Variation {variationSelection}
+            </button>
           </div>
         </div>
       </div>
