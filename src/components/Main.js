3617a5be87a3d63e76754df935ef1fc7a5f63de4
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2022-08-22 20:42:20 -0700

    add link to gravedigger

--- src/components/Main.js
+++ src/components/Main.js
@@ -322,7 +322,7 @@ const NftCollection = () => {
               <button
                 className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
       hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l"
-                onClick={alertClick1}
+      onClick={onClickUrl("https://gravedigger.app")}
               >
                 Enter The Cemetery
               </button>
