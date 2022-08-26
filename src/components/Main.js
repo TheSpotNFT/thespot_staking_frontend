7634d4ac51d8ef7e1e30772bcb4f9e689bafa93c
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2022-08-26 10:47:57 -0700

    add unnamed branding

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,8 @@ import analog from "../images/analogtitle.png";
 import apechain from "../images/apechain.png";
 import evolve from "../images/evolve/1/1.png";
 import cemetery from "../images/Cemetery.PNG"
+import unnamednft from "../images/logounnamed.png"
+
 
 const NftCollection = () => {
   const { Moralis } = useMoralis();
@@ -325,6 +327,25 @@ const NftCollection = () => {
       onClick={onClickUrl("https://gravedigger.app")}
               >
                 Enter The Cemetery
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full rounded overflow-hidden shadow-lg bg-slate-700 hover: hover:scale-105 hover:bg-slate-500 duration-300">
+        <img className="w-full" src={unnamednft} alt=""></img>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 flex justify-center">
+            <h1>Unnamed Branding</h1>
+          </div>
+          <div className="text-slate-50 text-base">
+            <div className="flex flex-col space-y-4 py-4">
+              <button
+                className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
+      hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l"
+      onClick={onClickUrl("https://unnamednft.app")}
+              >
+                Enter The Brandoooor
               </button>
             </div>
           </div>
