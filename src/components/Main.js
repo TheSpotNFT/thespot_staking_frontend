43eb4503430d81fcef314b2413fa4a3ea9fa25ef
TheSpotNFT <thespotnft@gmail.com>
2022-09-14 22:06:43 -0700

    swap cemetery and brandooor

--- src/components/Main.js
+++ src/components/Main.js
@@ -325,25 +325,7 @@ const NftCollection = () => {
           </div>
         </div>
       </div>
-      <div className="w-full rounded overflow-hidden shadow-lg bg-slate-700 hover: hover:scale-105 hover:bg-slate-500 duration-300">
-        <img className="w-full" src={cemetery} alt=""></img>
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2 flex justify-center">
-            <h1>The Cemetery</h1>
-          </div>
-          <div className="text-slate-50 text-base">
-            <div className="flex flex-col space-y-4 py-4">
-              <button
-                className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
-      hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l"
-      onClick={onClickUrl("https://gravedigger.app")}
-              >
-                Enter The Cemetery
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      
       <div className="w-full rounded overflow-hidden shadow-lg bg-slate-700 hover: hover:scale-105 hover:bg-slate-500 duration-300">
         <img className="w-full" src={unnamednft} alt=""></img>
         <div className="px-6 py-4">
@@ -358,6 +340,25 @@ const NftCollection = () => {
       onClick={onClickUrl("https://unnamednft.app")}
               >
                 Enter The Brandoooor
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full rounded overflow-hidden shadow-lg bg-slate-700 hover: hover:scale-105 hover:bg-slate-500 duration-300">
+        <img className="w-full" src={cemetery} alt=""></img>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 flex justify-center">
+            <h1>The Cemetery</h1>
+          </div>
+          <div className="text-slate-50 text-base">
+            <div className="flex flex-col space-y-4 py-4">
+              <button
+                className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
+      hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l"
+      onClick={onClickUrl("https://gravedigger.app")}
+              >
+                Enter The Cemetery
               </button>
             </div>
           </div>
